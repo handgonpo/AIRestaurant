bad7fb500b377782481745e9cbba7ee3d9f12a23
Youjung <handgonpo@naver.com>
2025-07-06 19:26:47 +0900

    Resolve merge conflict

--- proj/settings.py
+++ proj/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "NAME": os.environ.get("DB_NAME", "restaurant_db"),
         "USER": os.environ.get("DB_USER", "restaurant_user"),
         "PASSWORD": os.environ.get("DB_PASSWORD", "db_password"),
-        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "HOST": os.environ.get("DB_HOST", "127.0.0.1"),
         "PORT": os.environ.get("DB_PORT", "3306"),
         "OPTIONS": {"charset": "utf8mb4"},
     }
