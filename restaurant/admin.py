9275cf48dba863f8acdcdbec660430daa031e015
Youjung <handgonpo@naver.com>
2025-07-10 23:08:43 +0900

    Edit_Region

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import (
     Article,
+    CuisineType,
+    Region,
     Restaurant,
     RestaurantCategory,
     RestaurantImage,
@@ -15,7 +17,8 @@ from .models import (
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = [
+    # 관리자 목록 페이지에서 보여줄 컬럼을 지정함
+    list_display = [  # 장고가 공식적으로 제공하는 admin 옵션 변수
         "id",
         "title",
         "show_at_index",
@@ -23,7 +26,11 @@ class ArticleAdmin(admin.ModelAdmin):
         "created_at",
         "modified_at",
     ]
+
+    # 개별 객체를 추가하거나 수정할 때 보여줄 필드를 지정함 (편집 화면에서 보임)
+    # Admin에서 글을 생성하거나 수정할 때 이 필드들만 폼에 나타남
     fields = ["title", "preview_image", "content", "show_at_index", "is_published"]
+
     search_fields = ["title"]
     list_filter = ["show_at_index", "is_published"]
     date_hierarchy = "created_at"
@@ -61,6 +68,7 @@ class RestaurantAdmin(admin.ModelAdmin):
         "phone",
         "rating",
         "rating_count",
+        "region",
     ]
     fields = [
         "name",
@@ -72,13 +80,13 @@ class RestaurantAdmin(admin.ModelAdmin):
         "longitude",
         "tags",
     ]
-    readonly_fields = ["rating", "rating_count"]
+    readonly_fields = ["rating", "rating_count", "region"]
     search_fields = ["name", "branch_name"]
     list_filter = ["tags"]
     autocomplete_fields = ["tags"]
-    inlines = [RestaurantMenuInline, RestaurantImageInline]
+    inlines = [RestaurantMenuInline, RestaurantImageInline]  # 수정할때만 보여지는 폼
 
-    # 인스턴스를 생성할때 인라인 표시 안하도록
+    # 수정할 때만 인라인 폼을 보여주고, 새로 생성할 때는 인라인 폼을 숨기는 역할
     def get_inline_instances(self, request, obj=None):
         return obj and super().get_inline_instances(request, obj) or []
 
@@ -89,6 +97,16 @@ class RestaurantCategoryIAdmin(admin.ModelAdmin):
     fields = ["cuisine_type", "name"]
 
 
+@admin.register(CuisineType)
+class CuisineTypeAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    search_fields = ["name"]
+
+
+# 이 클래스들은 InlineModelAdmin, 즉 인라인 모델 등록용 클래스입니다.
+# RestaurantMenu는 Restaurant 객체를 수정할 때 같이 나타납니다.
+# 왼쪽 메뉴에 "RestaurantMenu"라는 항목은 아예 없습니다.
+# 대신, 레스토랑 수정 화면 안에 탭 형태로 들어가 있습니다.
 class ReviewImageInline(admin.TabularInline):
     model = ReviewImage
     extra = 1
@@ -108,3 +126,9 @@ class ReviewAdmin(admin.ModelAdmin):
 class SocialChannelAdmin(admin.ModelAdmin):
     list_display = ["id", "name"]
     fields = ["name"]
+
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("sido", "sigungu", "eupmyeondong")
+    search_fields = ("sido", "sigungu", "eupmyeondong")
