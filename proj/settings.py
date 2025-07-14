b895108e493b9b731454dd6ba4c282925c41948a
Youjung <handgonpo@naver.com>
2025-07-15 01:16:40 +0900

    mysql

--- proj/settings.py
+++ proj/settings.py
@@ -15,11 +15,11 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(dotenv_path=BASE_DIR / ".env", override=True)
+DB_NAME = os.environ.get("DB_NAME")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
