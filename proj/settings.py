5bb79850ffe177afa692ab8dece346ead648df25
Youjung <handgonpo@naver.com>
2025-07-19 21:15:21 +0900

    Refactor settings to use ALLOWED_HOSTS from env

--- proj/settings.py
+++ proj/settings.py
@@ -35,7 +35,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
