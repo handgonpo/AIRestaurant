1635541776dc46435fece485b661bdd058c12f57
Youjung <handgonpo@naver.com>
2025-07-19 13:26:02 +0900

    Add deploy

--- proj/settings.py
+++ proj/settings.py
@@ -25,7 +25,7 @@ DB_NAME = os.environ.get("DB_NAME")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "insecure-default-key-for-learning")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
